1d8ae75d47b08782f9053a117494b4e9ecd36125
souha hammadi <souhahammadi1@gmail.com>
2025-02-25 15:52:24 +0100

    second commit

--- ex2.java
+++ ex2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ex2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int nl,nc,i,j,somme;
+        int nl,nc,i,j;
         int[][] M=new int [30] [30];
         System.out.println("entrez le nombre de lignes");
         nl=sc.nextInt();
@@ -17,19 +17,22 @@ while(nc>30){
 System.out.println("entrez le nombre de colonnes");
 nc=sc.nextInt();
 }
+int somme=0;
+double produit=1;
+int total = nl * nc ;
 System.out.println("remplissez la matrice");
 for( i=0 ; i<nl ;i++){
     for(j=0;j<nc;j++){
             System.out.println("M["+i+"]["+j+"]=");
             M[i][j]=sc.nextInt();
-        
+        somme += M[i] [j];
+        produit *=  M[i] [j];
     }
 }
-somme=0;
-double produits=1;
-for( i=0 ; i<nl ;i++){
-    for(j=0;j<nc;j++){
-        somme+=M[i][j];
-        produits*=M[i][j];
-}}
-}}
+double moyenne = (double) somme / total;
+System.out.println("somme des élément :" +somme);
+System.out.println("produit des élément :" +produit);
+System.out.println("moyenne des élément :" +moyenne);
+    }}
+
+
