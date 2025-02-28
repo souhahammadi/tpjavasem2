7c9417a8a94437238532ac97bfdcd41cf853225f
souha hammadi <souhahammadi1@gmail.com>
2025-02-28 19:32:27 +0100

    new exerice

--- tp2 chaine/app1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class app1 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("entrez un chaine de caractere");
-        
-    }
-}
